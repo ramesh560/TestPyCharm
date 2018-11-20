fea1b7785b35852da5b494534f742b116fc8b369
ramesh <rawmesh.560@gmail.com>
2018-11-21 00:20:52 +0530

    updated

---
+++ TuplePython.py
@@ -0,0 +1,16 @@
+
+"""TuplePy = (6,7,33)
+Tup = (67,34,88)
+print(len(TuplePy))
+print(TuplePy[:2])
+print(TuplePy*2)
+print(TuplePy[2])
+print(TuplePy[-1])
+print(Tup)
+
+del Tup"""
+coordinates = (4,5)
+
+print(coordinates[0])
+
+
