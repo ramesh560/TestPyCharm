dfa0e2ef1cf572dbd76d8a542ea97a253f4ea664
ramesh <rawmesh.560@gmail.com>
2018-11-21 00:17:14 +0530

    updated

---
+++ Swap.py
@@ -0,0 +1,15 @@
+a = 50
+b = 60
+# swap program using third variable
+print (a,b)
+c  = a+b
+b: int = a
+a = c-b
+
+print (a,b)
+# swap program with no third variable
+a = a+b
+b = a-b
+a = a-b
+
+print(a,b)
